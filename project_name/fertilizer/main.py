17e2337e2995c5fd347abe3012099565383056b3
Jorapong <37506852+Jorapong@users.noreply.github.com>
2021-04-18 00:08:40 +0700

    add detail function fertilizer

--- project_name/fertilizer/main.py
+++ project_name/fertilizer/main.py
@@ -1,4 +1,6 @@
 from connectDB import ConnectDB
+from mqttsend import Mqttcon
+
 ConnectDB.connect()
 class Fertilizer:
     broker="127.0.0.1"
@@ -8,5 +10,16 @@ class Fertilizer:
         ec = ConnectDB.get_sensorvalue("ec",veget_id)
         ph = ConnectDB.get_sensorvalue("ph",veget_id)
         level = ConnectDB.get_sensorvalue("level",veget_id)
-        
+        mixer = ConnectDB.get_sensorvalue("mixer",veget_id)
+        if  (mixer == 1):
+            #ปล่อยนำทื้ง
+            Mqttcon.mqttconnect("@msg/pump/pump1","ON")
+            Mqttcon.mqttconnect("@msg/pump/pump1","OFF")
+        elif (mixer == 0):
+            level = 
+            fertilizerml=ec+ec #แก้สูตร
+            waterml=ec #แก้สูตร
+            Mqttcon.mqttconnect("@msg/fertilizer/fertilizer1/control",fertilizerml)
+            Mqttcon.mqttconnect("@msg/fertilizer/water/control",waterml)
+            Mqttcon.mqttconnect("@msg/pump/pump1","ON")
         return False
--- project_name/mqtt-subtoDBdevice.py
+++ project_name/mqtt-subtoDBdevice.py
@@ -11,23 +11,38 @@ topic_list = [
     ('@msg/greenHouse/IS/OPEN', 0),
     ('@msg/greenHouse/IS/CLOSE', 0),
     ('@msg/hydroponic/light/red', 0),
-    ('@msg/hydroponic/light/all', 0)
+    ('@msg/hydroponic/light/all', 0),
+    ('@msg/fertilizer/bug1/control', 0),
+    ('@msg/fertilizer/bug2/control', 0),
+    ('@msg/fertilizer/bug3/control', 0),
+    ('@msg/fertilizer/fertilizer1/control', 0),
+    ('@msg/fertilizer/fertilizer2/control', 0),
+    ('@msg/fertilizer/fertilizer3/control', 0),
 ]
 
 def on_message(client, userdata, message):
     msg = str(message.payload.decode("utf-8"))
     if (message.topic == topic_list[0][0]): 
         ConnectDB.set_status(0,"sunscreenOUT",0)
-    elif (message.topic == topic_list[1][1]):
+    elif (message.topic == topic_list[1][0]):
         ConnectDB.set_status(1,"sunscreenOUT",0)
-    elif (message.topic == topic_list[2][2]):
+    elif (message.topic == topic_list[2][0]):
         ConnectDB.set_status(0,"sunscreenIN",0)
-    elif (message.topic == topic_list[3][3]):
+    elif (message.topic == topic_list[3][0]):
         ConnectDB.set_status(1,"sunscreenIN",0)
-    elif (message.topic == topic_list[4][4]):
+    elif (message.topic == topic_list[4][0]):
         ConnectDB.set_status(1,"light",1111111)
-    elif (message.topic == topic_list[5][5]):
+    elif (message.topic == topic_list[5][0]):
         ConnectDB.set_status(0,"light",1111111)
+    elif (message.topic == topic_list[6][0] 
+    or message.topic == topic_list[7][0] 
+    or message.topic == topic_list[8][0]):
+        ConnectDB.set_status(1,"mixer",0)
+    elif (message.topic == topic_list[8][0]
+    or message.topic == topic_list[9][0]
+    or message.topic == topic_list[10][0]):
+        ConnectDB.set_status(0,"mixer",0)
+
     print("message received " ,msg)  
     print("message topic=",message.topic)
     print("message qos=",message.qos)
