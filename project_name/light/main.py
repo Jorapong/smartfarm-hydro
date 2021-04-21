8fb6e2258b3d27952be99d4d778c2b21ee2a3684
Jorapong <37506852+Jorapong@users.noreply.github.com>
2021-04-21 23:42:04 +0700

    test light

--- project_name/light/main.py
+++ project_name/light/main.py
@@ -9,32 +9,38 @@ class Light:
     def process_light(self, veget_id):
         light = self.get_light(veget_id)
         if light < 310 : #แสงมาก
-            Mqttcon.mqttconnect("@msg/hydroponic/light/all","OFF")
+            print('ปิดไฟ')
+            # Mqttcon.mqttconnect("@msg/hydroponic/light/all","OFF")
             if ConnectDB.get_status("sunscreenIN",0) == 0:
-                Mqttcon.mqttconnect("'@msg/greenHouse/OS/CLOSE","off")
-                Mqttcon.mqttconnect("@msg/greenHouse/OS/CLOSE","on")
+                print('ปิดสแลนด้านนอก')
+                # Mqttcon.mqttconnect("@msg/greenHouse/OS/CLOSE","off")
+                # Mqttcon.mqttconnect("@msg/greenHouse/OS/CLOSE","on")
                 #ClosesunscreenOUT
                 return True
             elif ConnectDB.get_status("sunscreenIN",0) == 1:
-                Mqttcon.mqttconnect("@msg/greenHouse/IS/CLOSE","off")
-                Mqttcon.mqttconnect("@msg/greenHouse/IS/CLOSE","on")
+                print('ปิดสแลนด้านใน')
+                # Mqttcon.mqttconnect("@msg/greenHouse/IS/CLOSE","off")
+                # Mqttcon.mqttconnect("@msg/greenHouse/IS/CLOSE","on")
                 #ClosesunscreenIN
                 return True
         elif light >335 : #แสงน้อย
             if ConnectDB.get_status("sunscreenOUT",0) == 1:
-                Mqttcon.mqttconnect("@msg/greenHouse/OS/OPEN","off")
-                Mqttcon.mqttconnect("@msg/greenHouse/OS/OPEN","on")
+                print('เปิดสแลนด้านนอก')
+                # Mqttcon.mqttconnect("@msg/greenHouse/OS/OPEN","off")
+                # Mqttcon.mqttconnect("@msg/greenHouse/OS/OPEN","on")
                 #ClosesunscreenOUT
                 return True
             elif ConnectDB.get_status("sunscreenOUT",0) == 0:
                 if ConnectDB.get_status("sunscreenIN",0) == 1:
-                    Mqttcon.mqttconnect("@msg/greenHouse/IS/OPEN","off")
-                    Mqttcon.mqttconnect("@msg/greenHouse/IS/OPEN","on")
+                    print('เปิดสแลนด้านใน')
+                    # Mqttcon.mqttconnect("@msg/greenHouse/IS/OPEN","off")
+                    # Mqttcon.mqttconnect("@msg/greenHouse/IS/OPEN","on")
                     #Closesunscreenin
                     return True
                 elif ConnectDB.get_status("sunscreenIN",0) == 0:
-                    Mqttcon.mqttconnect("@msg/hydroponic/light/red","off")
-                    Mqttcon.mqttconnect("@msg/hydroponic/light/blue","on")
+                    print('เปิดไฟ')
+                    # Mqttcon.mqttconnect("@msg/hydroponic/light/red","off")
+                    # Mqttcon.mqttconnect("@msg/hydroponic/light/blue","on")
                     #Open light
                     return True
         return False
