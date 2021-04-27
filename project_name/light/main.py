bff71bac12d34159dae05c3728cb149be3d51ad5
Jorapong <37506852+Jorapong@users.noreply.github.com>
2021-04-27 15:16:16 +0700

    ss

--- project_name/light/main.py
+++ project_name/light/main.py
@@ -68,7 +68,7 @@ class Light:
             sleep(1)
             client.publish("@msg/hydroponic/hydroponic3","on")
             sleep(1)
-            client.publish("@msg/pump/flow2",10000)#เติมปุ๋ยที่ผสมแล้ว
+            client.publish("@msg/pump/flow2",1000)#เติมปุ๋ยที่ผสมแล้ว
             # sleep(20)
             # client.publish("@msg/pump/flow2",5000)#เติมน้ำล้างท่อ
             # client.publish("@msg/htdroponic/htdroponic2","off")
