eae82846268dbf6f62fd8553573d21a7f3bcb166
Jorapong <37506852+Jorapong@users.noreply.github.com>
2021-04-29 22:40:40 +0700

    test main and sub

--- project_name/main_process.py
+++ project_name/main_process.py
@@ -7,22 +7,22 @@ from connectDB import ConnectDB
 ConnectDB.connect()
 def main():
     while True:
-        try :
-            ConnectDB._cursor.execute("SELECT * FROM veget")
-            myresult = ConnectDB._cursor.fetchall()
-            for value in myresult:
-                print('main')
-                veget_id=value['veget_id']
-                print('light work')
-                light_result = Light.process_light(veget_id)
-                print('fertilizer work')
-                fertilizer_result = Light.process_fertilizer(value)
-                print('light_result', light_result)
-                print('fertilizer_result', fertilizer_result)
-                sleep(60*10)
-            print('this is main process from Pi')
+        # try :
+        ConnectDB._cursor.execute("SELECT * FROM veget")
+        myresult = ConnectDB._cursor.fetchall()
+        for value in myresult:
+            print('main')
+            veget_id=value['veget_id']
+            print('light work')
+            light_result = Light.process_light(veget_id)
+            print('fertilizer work')
+            fertilizer_result = Light.process_fertilizer(value)
+            print('light_result', light_result)
+            print('fertilizer_result', fertilizer_result)
+            sleep(60*10)
+        print('this is main process from Pi')
             
-        except:
-            print('error main process')
+        # except:
+        #     print('error main process')
 if __name__ == '__main__':
     main()
